5087deb12c01ad78e73980f6ce8a2d7a85443ac2
shapskurts <shapskurts@users.noreply.github.com>
2019-09-19 15:03:26 +0200

    Add files via upload

---
+++ Bricks game/Game/Highscore.py
@@ -0,0 +1,14 @@
+# By Shepherd Kutama  shepherd.kutama@gmail.com
+class Highscore:
+
+    def __init__(self):
+        pass
+
+    def getScores(self):
+        pass
+
+    def load(self):
+        pass
+
+    def add(self, name, score):
+        pass
---
+++ Bricks game/Game/Pad.py
@@ -0,0 +1,7 @@
+# By Shepherd Kutama  shepherd.kutama@gmail.com
+from Game.Shared import *
+
+class Pad(GameObject):
+
+    def __init__(self, position, sprite):
+        super(Pad, self).__init__(position, GameConstants.PAD_SIZE, sprite)
